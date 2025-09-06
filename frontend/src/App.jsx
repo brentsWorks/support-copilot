d10eeeaa63f0400d749e1d758c34bebbc69a8327
Jason <zwu151@ucsc.edu>
2025-09-06 13:57:58 -0700

    setting up the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 dark:bg-gray-900">
+      <h1 className="text-4xl font-bold text-gray-900 dark:text-white">
+        Hello, World!
+      </h1>
+    </div>
+  )
+}
+
+export default App
